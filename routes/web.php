e809b1723bf64e8d5da218e3a90d9e8854efcf15
mustafa <mhajer51@netronsolutions.com>
2025-11-15 17:53:25 +0400

    Tests

--- routes/web.php
+++ routes/web.php
@@ -1,25 +1,2 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-
-Route::get('/', function () {
-    return view('dashboard', [
-        'page' => 'portal',
-        'mode' => 'login',
-    ]);
-})->name('portal.login');
-
-Route::view('/portal/dashboard', 'dashboard', [
-    'page' => 'portal',
-    'mode' => 'dashboard',
-])->name('portal.dashboard');
-
-Route::view('/admin/login', 'dashboard', [
-    'page' => 'admin',
-    'mode' => 'login',
-])->name('admin.login');
-
-Route::view('/admin', 'dashboard', [
-    'page' => 'admin',
-    'mode' => 'dashboard',
-])->name('admin.dashboard');
