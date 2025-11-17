aeb3273ee9ed5e85216770d81823963b48936c3c
mhajer51 <mhajer@netronsolutions.com>
2025-11-17 14:32:39 +0400

    Use local edit icon component

---
+++ resources/js/components/icons/EditOutlinedIcon.jsx
@@ -0,0 +1,10 @@
+import SvgIcon from '@mui/material/SvgIcon';
+
+const EditOutlinedIcon = (props) => (
+    <SvgIcon {...props}>
+        <path d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75z" />
+        <path d="M20.71 7.04a1 1 0 0 0 0-1.41l-2.34-2.34a1 1 0 0 0-1.41 0L14.13 4.12l3.75 3.75z" />
+    </SvgIcon>
+);
+
+export default EditOutlinedIcon;
